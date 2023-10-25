2174448eed59252bc24d2b5a4fe5812ac80cf44a
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-25 17:39:46 +0300

    updated removeWatchlist func

--- src/components/FavMovie.js
+++ src/components/FavMovie.js
@@ -1,12 +1,17 @@
 import { useDispatch } from "react-redux";
 import { removeWatchlist } from "../store/actions/watchlistActions";
+import { recycleMovie } from "../store/actions/movieActions";
 
-export default function FavMovie({ title, id }) {
+export default function FavMovie({ movie }) {
   const dispatch = useDispatch();
+  const removeWatchlistHandler = (movie) => {
+    dispatch(recycleMovie(movie))
+    dispatch(removeWatchlist(movie.id))
+  }
   return (
     <div className="flex p-3 pl-4 bg-white mb-2 shadow items-center group">
-      <div className="pr-4 flex-1">{title}</div>
-      <button onClick={() => dispatch(removeWatchlist(id))} className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100">
+      <div className="pr-4 flex-1">{movie.title}</div>
+      <button onClick={() => removeWatchlistHandler(movie) } className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100">
         Çıkar
       </button>
     </div>
