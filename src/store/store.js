5e6b52c02b84bdc46844c3ad5bf28828063409c1
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-25 16:06:11 +0300

    added redux-logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
-
+import logger from "redux-logger";
 import { reducers } from './reducers/index';
-import { myLogger } from "./middlewares/myLogger";
+//import { myLogger } from "./middlewares/myLogger";
 
-export const myStore = createStore(reducers, applyMiddleware(myLogger));
+export const myStore = createStore(reducers, applyMiddleware(logger));
